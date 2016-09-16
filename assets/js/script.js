abb159b12dddebb513bfa497bfd03bd48b62bb2b
justmartin <justin.lamar.martin@gmail.com>
2016-09-16 12:49:53 -0700

    Setup/linked html to css js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+/*Create an object called Multiplier with two methods: multiply and getCurrentValue. multiply should 
+initially return the number supplied * 1 and from then on whatever the current value is times the 
+number supplied. getCurrentValue should return the last answer returned from multiply.*/
