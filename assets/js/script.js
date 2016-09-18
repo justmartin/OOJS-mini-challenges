ae69be6b8baa22621231d0c8c39c03a2ee1a425e
justmartin <justin.lamar.martin@gmail.com>
2016-09-17 19:28:32 -0700

    Added title to html. Added js for mini challenge 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,13 +27,14 @@ console.log(Multiplier.getCurrentValue());
 console.log(Multiplier.multiply(2));
 
 console.log(Multiplier.getCurrentValue());
-*/
+
+/*-----------------------------------------------------------------------------------------------------------*/
 /*Implement an object model that allows you to store strings that represent a Photo. Your model should 
 include an Album object that can contain many Photo objects in its photos attribute. Each Album should 
 allow you to add a new photo, list all photos, and access a specific photo by the order it was added. 
 Each Photo should store the photo's file name and the location the photo was taken in as strings. Create 
 instances of each object defined to prove that your object model works.*/
-
+/*
 //creating a photo object that takes name and location. will be used to populate album object.
 function Photo(name, location) {
 	this.name = name;
@@ -70,38 +71,77 @@ Album.add_photo(photo_four);
 console.log(Album.list_photos());
 console.log(Album.photos);
 console.log(Album.select_photo(3));
-
-
+*/
+/*-----------------------------------------------------------------------------------------------------------*/
 /*Create a prototypical Person object. From this object, extend a Teacher object and a Student object. 
 Each of these objects should have attributes and methods pertinent to what they describe. Also create a 
 School object that should be able to store instances of students and teachers. Make sure to write code 
 afterwards that creates instances of these objects to make sure that what you've written works well and 
 you're able to store the necessary data in each object.*/
 
-/*
-function Person(firstName, lastName, email) {
+//defines Person object
+function Person(firstName, lastName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
-	this.email = email;
 }
 
-function Teacher() {
-	this.teacher = true;
-	Person.apply(this, arguments) // Call parent constructor with Person prototype context
+//defines Teacher object with extension of Person
+function Teacher(firstName, lastName, subject) {
+	Person.apply(this, arguments); 
+	this.subject = subject;
 }
 
-function Student() {
-	this.student = true;
-	Person.apply(this, arguments) // Call parent constructor with Person prototype context
+//defines Student object with extension of Person
+function Student(firstName, lastName, year) {
+	Person.apply(this, arguments);
+	this.year = year;
 }
 
-Teacher.prototype = new User(); //Admin now has all of the traits of a Person as well as its own teacher boolean flag
+//creates teachers and logs them to console
+var teacher1 = new Teacher("Justin", "Martin", "Math");
+console.log(teacher1);
 
-*/
+var teacher2 = new Teacher("Kyle", "Fugere", "Geography");
+console.log(teacher2);
 
+var teacher3 = new Teacher("Tyler", "Johns", "Phyiscal Education");
+console.log(teacher3);
 
+//creates students and logs them to console
+var student1 = new Student("Cody", "Kendrick", 10);
+console.log(student1);
 
+var student2 = new Student("Dustin", "Meade", 12);
+console.log(student2);
 
+var student3 = new Student("Alvin", "Marks", 11);
+console.log(student3);
 
+//defines School object consisting of two arrays and functions to populate those arrays
+var School = {
+	teachers: [],
+	students: [],
+
+	add_teacher: function(teacher) {
+		this.teachers.push(teacher);
+	},
+
+	add_student: function(student) {
+		this.students.push(student);
+	}
+};
+
+//adds teachers to teachers array
+School.add_teacher(teacher1);
+School.add_teacher(teacher2);
+School.add_teacher(teacher3);
+//logs teachers array
+console.log(School.teachers);
+//adds students to students array
+School.add_student(student1);
+School.add_student(student2);
+School.add_student(student3);
+//logs students array
+console.log(School.students);
 
 
